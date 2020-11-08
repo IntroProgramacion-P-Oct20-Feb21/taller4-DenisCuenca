202b0c8bfc1e2511beea71cada23c9ab5623e2a1
unknown <dacuenca7@utpl.edu.ec>
2020-11-07 19:25:14 -0500

    Código en java

--- EjerciciosCuatro/src/ejercicioscuatro/Ejercicio2.java
+++ EjerciciosCuatro/src/ejercicioscuatro/Ejercicio2.java
@@ -5,10 +5,49 @@
  */
 package ejercicioscuatro;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejercicio2 {
+    // 1.Inicio
+    public static void main(String[] args) {
+        
+        //  Declaro las varibles
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        //  2. hijo1, d[0,n]
+        double hijo1;
+        // 3. hijo2, d[0,n]
+        double hijo2;
+        // hijo3, d[0,n]
+        double hijo3;
+        // gastoTotal, d[0,n]
+        double gastoTotal;
     
+        //pido los datos por teclado- entradas
+        //6. <<"Ingrese los gastos del primer hijo"
+        System.out.println("Ingrese los gastos del primer hijo");
+        // 7. >>hijo1
+        hijo1 = entrada.nextDouble();
+        //8. <<"Ingrese los gastos del segundo hijo"
+        System.out.println("Ingrese los gastos del segundo hijo");
+        //9. >>hijo2
+        hijo2 = entrada.nextDouble();
+        // 10. <<"Ingrese los gastos del tercer hijo"
+        System.out.println("Ingrese los gastos del tercer hijo");
+        // 11. >>hijo3
+        hijo3 = entrada.nextDouble();
+        
+        //proceso
+        //12. gastoTotal <- hijo1 + hijo2 + hijo3
+        gastoTotal = hijo1 + hijo2 + hijo3;
+        // Salida
+        //13. <<"El gasto total es " + gastoTotal
+        System.out.printf("El gasto total es %.2f \n", gastoTotal);
+       
+    } // 14. Fin
 }
--- EjerciciosCuatro/src/ejercicioscuatro/Ejercicio5.java
+++ EjerciciosCuatro/src/ejercicioscuatro/Ejercicio5.java
@@ -5,10 +5,47 @@
  */
 package ejercicioscuatro;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejercicio5 {
+    // 1. Inicio
+    public static void main(String[] args) {
+        // Declaro las variables
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        // 2.  montoPrestamo, d[0,n]
+        double montoPrestamo;
+        // 3.  interesMensual, d[0,n]
+        double interesMensual;
+        // 4. montoMensual, d[0,n]
+        double montoMensual;
+        // 5. totalMensual, d[0,n]
+        double totalMensual;
+        
+        // Pido los datos por teclado- entradas
+        // 6. << "Ingrese el monto del prestamo"
+        System.out.println("Ingrese el monto del prestamo");
+        // 7. >> montoPrestamo
+        montoPrestamo = entrada.nextDouble();
+        //8. << "Ingrese el interes mensual a cobrar"
+        System.out.println("Ingrese el interes mensual a cobrar");
+        // 9. >> interesMensual
+        interesMensual = entrada.nextDouble();
+        // Proceso
+        //10. montoMensual <-- montoPestamo/12
+       montoMensual = montoPrestamo/12;
+       // 11. totalMensual <-- MontoMensual + interesMensual
+       totalMensual = montoMensual + interesMensual;
+       
+       // Salidas
+       //12. << "El valor mensual a pagar es: " + totalMensual
+        System.out.printf("El valor mensual a pagar es: %.2f\n", totalMensual);
+
+    } // Fin
     
 }
